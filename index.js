1229e6bd84d474b69ff1dfab63424ba05a7e74c1
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-07-15 22:53:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards( names, event ) {
+  let thanksCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    thanksCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return thanksCards
+}
+
+function countdown(countdown) {
+  while (countdown > 0) {
+    console.log(countdown);
+    countdown -= 1;
+  }
+  console.log(countdown);
+}
